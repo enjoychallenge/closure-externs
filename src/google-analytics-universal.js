5ffd0329dd4557898b4bd64f2d48a1ce1eed77fa
Jiri Kozel <jiri.kozel.cz@gmail.com>
2014-04-24 11:13:08 +0200

    Google Analytics Universal added

---
+++ src/google-analytics-universal.js
@@ -0,0 +1,11 @@
+/**
+ * @fileoverview Externs for the Google Analytics Universsl.
+ * @externs
+ */
+
+/**
+ * @param {string} p1
+ * @param {string=} opt_p2
+ * @param {string=} opt_p3
+ */
+var ga = function(p1, opt_p2, opt_p3) {};
