e0f2ce05d93f4cee27c63a5038df918b72e93027
Jiri Kozel <jiri.kozel.cz@gmail.com>
2014-04-24 11:05:15 +0200

    browser-update added

---
+++ src/browser-update.js
@@ -0,0 +1,25 @@
+/**
+ * @fileoverview Externs for the browser-update.org.
+ * @externs
+ */
+
+/**
+ * @type {*}
+ */
+var $bu = {};
+
+/**
+ * @param {...*} var_args
+ */
+var $buo = function(var_args) {};
+
+/**
+ * @type {Object}
+ */
+var $buoop = {};
+
+/**
+ * @type {Object}
+ */
+var op = {};
+
