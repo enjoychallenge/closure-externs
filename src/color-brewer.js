31c6c9d17fe7d34371d0370feb613528a9871265
unknown <jirik@ANTON.dis.ics.muni.cz>
2014-06-24 16:16:15 +0200

    color brewer

---
+++ src/color-brewer.js
@@ -0,0 +1,13 @@
+/**
+ * @fileoverview Externs for ColorBrewer:
+ * http://colorbrewer2.org/
+ * @externs
+ */
+
+var colorbrewer = {};
+
+/**
+ * @type {Object.<number,Array.<string>>}
+ */
+colorbrewer.YlOrRd;
+
