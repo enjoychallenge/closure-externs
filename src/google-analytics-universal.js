799e28e9c5aa1c816cb707eec370916948447fc8
Jiri Kozel <jirik@users.noreply.github.com>
2016-01-07 10:15:05 +0100

    Update google-analytics-universal.js

--- src/google-analytics-universal.js
+++ src/google-analytics-universal.js
@@ -7,5 +7,8 @@
  * @param {string} p1
  * @param {string=} opt_p2
  * @param {string=} opt_p3
+ * @param {string=} opt_p4
+ * @param {string=} opt_p5
+ * @param {number=} opt_p6
  */
-var ga = function(p1, opt_p2, opt_p3) {};
+var ga = function(p1, opt_p2, opt_p3, opt_p4, opt_p5, opt_p6) {};
